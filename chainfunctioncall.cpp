b8d68af0bb8d735cfd87675bacfd1848d68be20c
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-04 16:55:05 +0700

    Menambahkan method chaining pada setter class Buku

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+class buku
+{
+    string judul;
+
+public:
+    buku &setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this;   //chain fuction
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+};
