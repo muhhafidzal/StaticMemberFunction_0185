ea1d08a2edced38f1eb3281df6c23780c6961b48
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-04 16:40:12 +0700

    Menambahkan pemanggilan constructor berparameter di main

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -13,3 +13,8 @@ mahasiswa::mahasiswa(int nim, string nama) {
     cout << "NIM :" << nim << endl;
     cout << "Nama :" << nama << endl;
 }
+
+int main (){
+    mahasiswa mhs(12345, "Pascal"); //memanggil construtor dengan parameter
+    return 0;
+}
