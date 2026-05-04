749d20f1a07eea4d5d3ba2a02867502df83f48d0
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-04 16:23:08 +0700

    Menambahkan atribut, constructor, dan method pada class Mahasiswa

--- staticmember.cpp
+++ staticmember.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa { //membuat class mahasiswa
+class Mahasiswa { //membuat class mahasiswa
 
-}
+public:
+    static int nim; //deklarasi variabel static yaitu variabel nim
+    //deklarasi variabel member
+    int id;
+    string nama;
+
+    //deklarasi prosedur setID() dan printALL()
+    void setID();
+    void printALL();
+
+    //pembuatan contructor Mahasiswa dengan parameter pnama
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    }
+};
