81c746b2e3c84e9b3d103787d8b2cc66bde28086
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-04 16:33:37 +0700

    Menambahkan penggunaan static method setNim dan getNim di main

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -39,3 +39,27 @@ void Mahasiswa::printALL() {
     cout << "Nama = " << nama << endl;
     cout << endl;
 }
+
+
+int main () {
+
+    //pembuatan object dan pemberian nilai
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+    //memberi nilai pada setNim() untuk merubah nilai NIM
+    Mahasiswa::setNim(9);
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+
+    //memanggil prosedur printALL()
+    mhs1.printALL();
+    mhs2.printALL();
+    mhs3.printALL();
+    mhs4.printALL();
+
+    //menampilkan NIM terakhir yang diproses
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
+    system("pause");
+
+    return 0;
+}
