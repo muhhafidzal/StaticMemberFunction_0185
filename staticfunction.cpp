e303a0e95668f4dd912fe1ddfcd9b4a3abf9b250
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-04 16:29:44 +0700

    Menambahkan fungsi static setNim dan getNim

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -3,4 +3,26 @@
 using namespace std;
 
 class Mahasiswa {
-}
+
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printALL();
+
+    //pembuatan static function
+    static void setNim(int pnim) {
+        nim = pnim;       
+    };
+
+    static int getNim() {
+        return nim;
+    }
+
+    Mahasiswa(string pnama) :nama(pnama) {
+        setID();
+    };
+};
